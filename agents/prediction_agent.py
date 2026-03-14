158288564d699bdbc7ce3c3e1ba866684b9aaec4
ridhima-2412 <ridhimanagpal2412@gmail.com>
2026-03-14 19:59:26 +0530

    Update prediction_agent.py

--- agents/prediction_agent.py
+++ agents/prediction_agent.py
@@ -1,1 +1,12 @@
+class PredictionAgent:
 
+    def predict_cost(self, metrics):
+
+        estimated_cost = 0
+
+        for instance in metrics:
+            estimated_cost += 50
+
+        print("Prediction Agent: Estimated monthly cost calculated")
+
+        return estimated_cost
