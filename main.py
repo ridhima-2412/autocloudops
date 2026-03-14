946d3874b6db285d3e215a01c510ac392ff82200
ridhima-2412 <ridhimanagpal2412@gmail.com>
2026-03-14 20:00:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+from agents.monitoring_agent import MonitoringAgent
+from agents.analysis_agent import AnalysisAgent
+from agents.prediction_agent import PredictionAgent
+from agents.optimization_agent import OptimizationAgent
 
+def main():
+
+    monitor = MonitoringAgent()
+    analyzer = AnalysisAgent()
+    predictor = PredictionAgent()
+    optimizer = OptimizationAgent()
+
+    metrics = monitor.collect_metrics("data/sample_metrics.json")
+
+    inefficient = analyzer.analyze_resources(metrics)
+
+    cost = predictor.predict_cost(metrics)
+
+    recommendations = optimizer.recommend(inefficient)
+
+    print("\nEstimated Monthly Cost:", cost)
+
+    print("\nOptimization Suggestions:")
+    for r in recommendations:
+        print("-", r)
+
+
+if __name__ == "__main__":
+    main()
