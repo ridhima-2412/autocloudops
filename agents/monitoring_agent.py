912c0748d91f4c5da2678fa93a1daec9ea933f5e
ridhima-2412 <ridhimanagpal2412@gmail.com>
2026-03-14 19:58:28 +0530

    Update monitoring_agent.py

--- agents/monitoring_agent.py
+++ agents/monitoring_agent.py
@@ -1,1 +1,10 @@
+import json
 
+class MonitoringAgent:
+
+    def collect_metrics(self, filepath):
+        with open(filepath, "r") as file:
+            data = json.load(file)
+
+        print("Monitoring Agent: Metrics collected")
+        return data
