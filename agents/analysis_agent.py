c25ecf3d82708da742103a242eda02a11dd92fe4
ridhima-2412 <ridhimanagpal2412@gmail.com>
2026-03-14 19:57:59 +0530

    Update analysis_agent.py

--- agents/analysis_agent.py
+++ agents/analysis_agent.py
@@ -1,1 +1,12 @@
+class AnalysisAgent:
 
+    def analyze_resources(self, metrics):
+
+        inefficient_instances = []
+
+        for instance in metrics:
+            if instance["cpu_usage"] < 15:
+                inefficient_instances.append(instance)
+
+        print("Analysis Agent: Inefficient resources detected")
+        return inefficient_instances
